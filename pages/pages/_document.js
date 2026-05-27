54079874cffde567a6a11da08a3a735d10e2078d
calmtowns <calmtowns@gmail.com>
2026-05-27 20:22:05 +0300

    Update _document.js

--- pages/pages/_document.js
+++ pages/pages/_document.js
@@ -1,13 +1,3 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
 export default function Document() {
-  return (
-    <Html lang="ru">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+  return null
 }
